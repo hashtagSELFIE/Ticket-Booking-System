930e4b4f8a2236b247312adc7ab7c05cce16ccf3
htselfie <james333james.ct@gmail.com>
2020-04-23 18:48:31 +0700

    migrate models

--- booking/models.py
+++ booking/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Ticket(models.Model):
-    buyer = models.ForeignKey('', on_delete=models.CASCADE)
-    boardingTrain = models.ForeignKey('', on_delete=models.CASCADE)
+    # buyer = models.ForeignKey('', on_delete=models.CASCADE)
+    # boardingTrain = models.ForeignKey('', on_delete=models.CASCADE)
     transactionInformation = models.TextField()
     transactionStatus = models.BooleanField()
     buyDate = models.DateField()
