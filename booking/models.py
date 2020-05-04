88c9d125211348eb1b2c234458d5db3e5d4d6c8d
Icyscools <meet.ice1@gmail.com>
2020-05-05 02:02:53 +0700

    Change Ticket model variable case and add travel_date

--- booking/models.py
+++ booking/models.py
@@ -6,12 +6,13 @@ from schedule.models import Train, Timetable, Station
 # Create your models here.
 class Ticket(models.Model):
     buyer = models.ForeignKey(Account, on_delete=models.CASCADE)
-    boardingTrain = models.ForeignKey(Train, on_delete=models.CASCADE)
-    fromStation = models.ForeignKey(
+    boarding_train = models.ForeignKey(Train, on_delete=models.CASCADE)
+    from_station = models.ForeignKey(
         Timetable, on_delete=models.CASCADE, related_name="fromStation")
-    toStation = models.ForeignKey(
+    to_station = models.ForeignKey(
         Timetable, on_delete=models.CASCADE, related_name="toStation")
-    transactionInformation = models.TextField(default='')
-    transactionStatus = models.BooleanField(default=False)
-    buyDate = models.DateField(auto_now_add=True)
-    bookingStatus = models.BooleanField(default=False)
+    transaction_information = models.TextField(default='')
+    transaction_status = models.BooleanField(default=False)
+    buy_date = models.DateField(auto_now_add=True)
+    travel_date = models.DateField(auto_now_add=True)
+    booking_status = models.BooleanField(default=False)
